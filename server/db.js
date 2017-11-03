e83cde68d5048bb8405c8124be348f58d92001e3
xcbt9527 <admin520>
2017-11-03 14:00:19 +0800

    new add vue profile

--- server/db.js
+++ server/db.js
@@ -1,13 +1,13 @@
-/**
- * Created by baird on 17/11/3.
- */
-// 数据库连接配置
-module.exports = {
-  mysql: {
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'test',
-    port: '3306'
-  }
-}
+// /**
+//  * Created by baird on 17/11/3.
+//  */
+// // 数据库连接配置
+// module.exports = {
+//   mysql: {
+//     host: 'localhost',
+//     user: 'root',
+//     password: '123456',
+//     database: 'test',
+//     port: '3306'
+//   }
+// }
