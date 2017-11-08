0569d8fb2360ede52024ba265e4b74e22346792b
xcbt9527 <admin520>
2017-11-08 14:39:44 +0800

    新增可访问静态vue文件

--- server/db.js
+++ server/db.js
@@ -1,10 +1,13 @@
+/**
+ * Created by baird on 17/11/8.
+ */
 // 数据库连接配置
 module.exports = {
-    mysql: {
-        host: 'localhost',
-        user: 'root',
-        password: '123456',
-        database: 'test',
-        port: '3306'
-    }
+  mysql: {
+    host: '47.94.157.0',
+    user: 'itoffice',
+    password: 'admin520',
+    database: 'momo',
+    port: '3306'
+  }
 }
